1dd3e0956ad130b2c861b7b6ba0192d838a20aec
yasin <buntyvirani82@gmail.com>
2021-01-30 06:32:51 -0800

    body parser added and getting data using api successfully

--- ecommerce-back-end/src/index.server.js
+++ ecommerce-back-end/src/index.server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const env = require("dotenv");
 const app = express();
+const bodyParser = require("body-parser");
 
 //enviroment variable or u can say constant
 env.config();
+
+app.use(bodyParser());
 
 app.get("/", (req, res, next) => {
   res.status(200).json({
