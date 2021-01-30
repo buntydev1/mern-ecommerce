827f6794039bdc0e525592c301fe3aa2b91a44ba
yasin <buntyvirani82@gmail.com>
2021-01-30 04:05:54 -0800

    first api created for resquest

--- ecommerce-back-end/src/index.server.js
+++ ecommerce-back-end/src/index.server.js
@@ -5,6 +5,12 @@ const app = express();
 //enviroment variable or u can say constant
 env.config();
 
+app.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Hello from server",
+  });
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
