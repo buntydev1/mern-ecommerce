9361998fe1831a8fc128f872aa93edc13fe97693
yasin <buntyvirani82@gmail.com>
2021-02-01 00:05:47 -0800

    database connected

--- ecommerce-back-end/src/index.server.js
+++ ecommerce-back-end/src/index.server.js
@@ -2,9 +2,23 @@ const express = require("express");
 const env = require("dotenv");
 const app = express();
 const bodyParser = require("body-parser");
-
+const mongoose = require("mongoose");
 //enviroment variable or u can say constant
 env.config();
+
+//mongodb connection
+//mongodb+srv://root:<password>@cluster0.a4tt1.mongodb.net/<dbname>?retryWrites=true&w=majority
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.a4tt1.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Database connected");
+  });
 
 app.use(bodyParser());
 
