c94309bf53158be8a99cf3a2f95f1aeef20401ae
yasin <buntyvirani82@gmail.com>
2021-02-03 05:05:44 -0800

    setting up the router and post call

--- ecommerce-back-end/src/index.server.js
+++ ecommerce-back-end/src/index.server.js
@@ -6,6 +6,8 @@ const mongoose = require("mongoose");
 //enviroment variable or u can say constant
 env.config();
 
+const userRoutes = require("./routes/user");
+
 //mongodb connection
 //mongodb+srv://root:<password>@cluster0.a4tt1.mongodb.net/<dbname>?retryWrites=true&w=majority
 mongoose
@@ -21,18 +23,7 @@ mongoose
   });
 
 app.use(bodyParser());
-
-app.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "Hello from server",
-  });
-});
-
-app.post("/data", (req, res, next) => {
-  res.status(200).json({
-    message: req.body,
-  });
-});
+app.use("/api", userRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
