088dd7452710af486d1b7f6679c9cea5f6de7602
yasin <buntyvirani82@gmail.com>
2021-02-03 06:09:12 -0800

    bcrypt added

--- ecommerce-back-end/src/models/user.js
+++ ecommerce-back-end/src/models/user.js
@@ -51,4 +51,9 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.virtual("password").set(function (password) {
+  // this.hash_password =
+});
+
 module.exports = mongoose.model("user", userSchema);
