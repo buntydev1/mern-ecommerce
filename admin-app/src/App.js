fb5b6682b2c1d2f4f668b266530d004f17df441b
yasin <buntyvirani82@gmail.com>
2021-02-06 03:08:50 -0800

    Router setted for pages

--- admin-app/src/App.js
+++ admin-app/src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import Home from "./container/Home";
+import Signin from "./container/Signin";
+import Signup from "./container/Signup";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/signin" component={Signin}></Route>
+          <Route path="/signup" component={Signup}></Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ admin-app/src/container/Home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Jumbotron } from "react-bootstrap";
+import Layouts from "../../components/Layouts";
+
+const Home = (props) => {
+  return (
+    <Layouts>
+      <Jumbotron>
+        <h1>welcome to admin</h1>
+      </Jumbotron>
+    </Layouts>
+  );
+};
+export default Home;
